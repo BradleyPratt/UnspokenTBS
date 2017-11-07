c45bd2f5f3af20e3139d6b7e73995c64759d0bc0
786Azeem <azeemali786@hotmail.com>
2017-11-07 11:34:25 +0000

    Azeems changes

---
+++ UnspokenTBS/Assets/Health Bar/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public float TotalHp;
+    public float CurrentHp;
+
+	// Use this for initialization
+	void Start ()
+    {
+        CurrentHp = TotalHp;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            TakeDamage();
+        }
+	}
+
+    void TakeDamage()
+    {
+        CurrentHp -= 5;
+        transform.localScale = new Vector3((CurrentHp / TotalHp), 1, 1);
+    }
+}
---
+++ UnspokenTBS/Assets/Health Bar/TurnManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour
+{
+    public int turnStatus = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void CurrentTurn()
+    {
+        if (turnStatus == 0)
+        {
+            playerTurn();
+        }
+        else if (turnStatus == 1)
+        {
+            playerTwoTurn();
+        }
+    }
+
+
+    void playerTurn()
+    {
+        Debug.Log("Player's turn");
+    }
+
+    void playerTwoTurn()
+    {
+        Debug.Log("Player Two's turn");
+    }
+
+    public void TurnChange()
+    {
+        if (turnStatus == 0)
+        {
+            turnStatus = 1;
+        }
+        else if (turnStatus == 1)
+        {
+            turnStatus = 0;
+        }
+    }
+}
