def4f5e43b21bccfd6949400b86e2d20b34bffb0
Joshua Livermore (MortalArrow) <u1653259@unimail.hud.ac.uk>
2017-11-09 03:31:17 +0000

    Can now move unit by clicking on new location if it is within the unit's range.

--- UnspokenTBS/Assets/UnitMovement.cs
+++ UnspokenTBS/Assets/UnitMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class UnitMovement : MonoBehaviour {
 
+	bool unitSelected = true;
+	[SerializeField]
+	Camera camera;
+	[SerializeField]
+	float moveRangeLimit;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,22 @@ public class UnitMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (unitSelected) {
+			if (Input.GetMouseButtonDown (0)) {
+				RaycastHit hit;
+				Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+				Physics.Raycast (ray.origin, ray.direction, out hit);
+				Vector3 newPosition = ray.origin + ray.direction * hit.distance;
+				if (Vector3.Distance (newPosition, transform.position) <= moveRangeLimit) {
+					transform.position = newPosition;
+				}
+			}
+		}
 	}
+
+	// Todo - replace with automatic selection from the turn system.
+	void OnMouseDown() {
+		//unitSelected = !unitSelected;
+	}
+
 }
