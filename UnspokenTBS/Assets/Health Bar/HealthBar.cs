c1eb2745886347779e919c033a930f3a1b9d0e30
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2017-11-10 00:29:07 +0000

    Health Bars finished

--- UnspokenTBS/Assets/Health Bar/HealthBar.cs
+++ UnspokenTBS/Assets/Health Bar/HealthBar.cs
@@ -6,24 +6,63 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour {
 
     public float totalHp = 100;
-    public float currentHp;
-    public Slider healthSlider;
+    public float currentHp; 
+    public Slider healthSlider; //HealthSlider, needed for creating Health Sliders
+    GameObject[] units; 
+    public Slider healthBar; //Only needed for instantiation
+    static int numOfUnits;
 
     // Use this for initialization
     void Start ()
     {
+        Debug.Log( "started again" );
         currentHp = totalHp;
-	}
+
+        BuildUnits();
+        Debug.Log( "done done done" );
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        /* var wantedPos = Camera.main.WorldToScreenPoint( position );
+         transform.position=wantedPos;*/
+         if (currentHp<=0) {
+            Destroy( gameObject );
+        }
     }
 
     public void TakeDamage(float damage)
     {
-        currentHp -= damage;
-        healthSlider.value = currentHp;
+        currentHp-=damage;
+        healthSlider.value=currentHp;
     }
-   
+
+    private void BuildUnits() {
+        units=GameObject.FindGameObjectsWithTag( "Unit" );
+        Debug.Log( "num of units= " + numOfUnits );
+        for (int i = numOfUnits; i<units.Length; i++) {
+            numOfUnits++;
+            GameObject unit;
+            unit=units[i];
+            Debug.Log( "i= " + i );
+            Debug.Log("units length= " + units.Length );
+
+            Vector3 position = new Vector3(unit.transform.position.x, unit.transform.position.y+0.75f, unit.transform.position.z);
+
+            Debug.Log( "Player Number "+i+" is named "+units[i].name );
+
+            InstantiateHealthBar(unit, position);
+            Debug.Log( "done done" );
+        }
+    }
+
+    private void InstantiateHealthBar( GameObject unit, Vector3 position ) {
+        Slider gameObjHealthBar = Instantiate( healthBar, position, Camera.main.transform.rotation ) as Slider;
+        unit.AddComponent<HealthBar>().healthSlider=gameObjHealthBar;
+        Canvas canvas = FindObjectOfType<Canvas>();
+        gameObjHealthBar.transform.SetParent( canvas.transform );
+
+        Debug.Log( "Done" );
+    }
 }
--- UnspokenTBS/Assets/Health Bar/TakeDamage.cs
+++ UnspokenTBS/Assets/Health Bar/TakeDamage.cs
@@ -2,35 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Take damage method
+*/
 public class TakeDamage : MonoBehaviour
 {
     public float damage = 1;
-    HealthBar healthBar;
-    GameObject[] units;
+    //HealthBar healthBar;
 
 	// Use this for initialization
 	void Start () {
-        units=GameObject.FindGameObjectsWithTag( "Unit" );
-        //healthBar=units.GetComponent<HealthBar>();
 
-        for (int i = 0; i<units.Length; i++) {
-            GameObject unit;
-            unit = units[i];
-
-            Debug.Log( "Player Number "+i+" is named "+units[i].name );
-
-            healthBar=unit.GetComponent<HealthBar>();
-        }
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetMouseButtonDown( 0 )) { // if left button pressed...
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+            RaycastHit hit;
+            Debug.Log( "he shoots" );
 
-        if (GameObject.FindWithTag("Unit"))
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                healthBar.TakeDamage(damage);
+            if (Physics.Raycast( ray, out hit )) {
+                HealthBar healthBar = hit.collider.GetComponent<HealthBar>();
+                Debug.Log( "he scores" );
+                if (hit.transform.gameObject.tag=="Unit") {
+                    healthBar.TakeDamage( damage );
+                    Debug.Log( "he wins" );
+                }
             }
         }
 
