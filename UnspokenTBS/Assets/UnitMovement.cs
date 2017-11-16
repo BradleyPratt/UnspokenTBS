17fd521ffb6de64a1a5bf679a823f0bcd47e8dea
Joshua Livermore (MortalArrow) <u1653259@unimail.hud.ac.uk>
2017-11-16 10:42:18 +0000

    Slight change to unit movement; can now only move on terrain. Now finds all units in attack range.

--- UnspokenTBS/Assets/UnitMovement.cs
+++ UnspokenTBS/Assets/UnitMovement.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class UnitMovement : MonoBehaviour {
 
-	bool unitSelected = true;
+	// Has the unit been selected by the turn counter? i.e. is it this unit's turn
+	bool unitSelected = false;
+
+	// Has the unit moved yet?
+	bool hasMoved = false;
+
+	// Should be set to be the camera used when the player moves units
 	[SerializeField]
 	Camera rayCamera;
+
+	// The max distance the unit can move per turn. Set to 0 for no movement
 	[SerializeField]
 	float moveRangeLimit;
+
+	// The min and max attack ranges for the unit
+	[SerializeField]
+	Vector2 attackRange;
+
+	// The radius of the sphere in which units will take damage
+	[SerializeField]
+	float attackRadius;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,27 +33,54 @@ public class UnitMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (unitSelected) {
+		if (unitSelected && !hasMoved) {
 			if (Input.GetMouseButtonDown (0)) {
 				RaycastHit hit;
 				Ray ray = rayCamera.ScreenPointToRay (Input.mousePosition);
 				Physics.Raycast (ray.origin, ray.direction, out hit);
 				Vector3 newPosition = ray.origin + ray.direction * hit.distance;
-				if (Vector3.Distance (newPosition, transform.position) <= moveRangeLimit) {
+				if ((Vector3.Distance (newPosition, transform.position) <= moveRangeLimit) && (hit.collider.CompareTag("Terrain"))) {
 					transform.position = newPosition;
+					hasMoved = true;
+				}
+			}
+		} else if (unitSelected && hasMoved) {
+			// todo make another projecter which shows attack
+			if (Input.GetMouseButtonDown (0)) {
+				RaycastHit hit;
+				Ray ray = rayCamera.ScreenPointToRay (Input.mousePosition);
+				Physics.Raycast (ray.origin, ray.direction, out hit);
+				Vector3 target = ray.origin + ray.direction * hit.distance;
+				if (attackRange.x <= Vector3.Distance (target, transform.position) <= attackRange.y) {
+					// Create array of colliders in attack range
+					Collider[] colliderArray = Physics.OverlapSphere(target, attackRadius);
+
+					foreach(Collider tempCollider in colliderArray){
+						// check if colliders are units
+						if (tempCollider.CompareTag ("Unit")) {
+							//todo apply damage
+						}
+					}
 				}
 			}
 		}
 	}
 
+	// Tell the script it's this unit's turn
 	public void SetUnitTurn(bool turnStatus) {
+		if (turnStatus) {
+			// todo add projecter here to display range arround the unit with a circle
+			hasMoved = false;
+		}
 		unitSelected = turnStatus;
 	}
 
+	// returns if it is this unit's turn
 	public bool GetUnitTurn() {
 		return unitSelected;
 	}
 
+	// Sets new movement range for unit; returns false if new range was below 0
 	public bool SetMovementRange(float newRange){
 		if (newRange >= 0) {
 			moveRangeLimit = newRange;
@@ -46,6 +90,7 @@ public class UnitMovement : MonoBehaviour {
 		}
 	}
 
+	// returns current movement range
 	public float GetMovementRange() {
 		return moveRangeLimit;
 	}
