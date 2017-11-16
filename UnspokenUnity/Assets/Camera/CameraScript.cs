62a1570bddedfb7b6a067a3319437a0be1417f1a
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2017-11-16 15:31:57 +0000

    New Project

---
+++ UnspokenUnity/Assets/Camera/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public float speed = 0.5f;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey( KeyCode.RightArrow  ) || Input.GetKey( KeyCode.D )) {
+            transform.position=new Vector3( transform.position.x+speed, transform.position.y, transform.position.z );
+        }
+        if (Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.A )) {
+            transform.position=new Vector3( transform.position.x-speed, transform.position.y, transform.position.z );
+        }
+        if (Input.GetKey( KeyCode.DownArrow ) || Input.GetKey( KeyCode.S )) {
+            transform.position=new Vector3( transform.position.x, transform.position.y, transform.position.z-speed );
+        }
+        if (Input.GetKey( KeyCode.UpArrow ) || Input.GetKey ( KeyCode.W)) {
+            transform.position=new Vector3( transform.position.x, transform.position.y, transform.position.z+speed );
+        }
+    }
+}
