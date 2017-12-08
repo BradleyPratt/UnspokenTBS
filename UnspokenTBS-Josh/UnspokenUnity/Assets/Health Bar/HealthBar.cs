489981e56ae53936fea730ada567f73b0079ab41
Saifuddeen Van Der Meer de Rothschild <33056601+TheNeverSOFT@users.noreply.github.com>
2017-12-08 16:50:26 +0000

    Level Design Complete
    Placed the current assets around to form both USA and USSR bases

---
+++ UnspokenTBS-Josh/UnspokenUnity/Assets/Health Bar/HealthBar.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public float totalHp = 100;
+    public float currentHp; 
+    Slider healthSlider; //HealthSlider, needed for creating Health Sliders
+    GameObject[] units; 
+    public Slider healthBar; //Only needed for instantiation
+    static int numOfUnits;
+
+    // Use this for initialization
+    void Start ()
+    {
+        currentHp = totalHp;
+
+        BuildUnits();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //BuildUnits();
+        if (numOfUnits < GameObject.FindGameObjectsWithTag("Unit").Length)
+        {
+            BuildUnits();
+            Debug.Log("Ran buildunits()");
+        }
+
+        if (healthSlider != null)
+        {
+            healthSlider.transform.position = new Vector3(gameObject.transform.position.x-1f, gameObject.transform.position.y-0.5f, gameObject.transform.position.z);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHp-=damage;
+        healthSlider.value = currentHp;
+		if (currentHp <= 0)
+		{
+			gameObject.tag = ("Untagged");
+			numOfUnits--;
+			GameObject.FindGameObjectWithTag("GameManager").GetComponent<TurnManager>().RemoveUnit(gameObject);
+			Destroy(gameObject);
+		}
+	}
+
+    private void BuildUnits() {
+        units=GameObject.FindGameObjectsWithTag( "Unit" );
+
+        for (int i = numOfUnits; i<units.Length; i++) {
+            numOfUnits++;
+            GameObject unit;
+            unit=units[i];
+
+            Vector3 position = new Vector3( unit.transform.position.x-1f, unit.transform.position.y, unit.transform.position.z );
+
+            InstantiateHealthBar( unit, position );
+        }
+    }
+
+    private void InstantiateHealthBar( GameObject unit, Vector3 position ) {
+        Slider gameObjHealthBar = Instantiate( healthBar, position, Camera.main.transform.rotation ) as Slider;
+        unit.AddComponent<HealthBar>().healthSlider=gameObjHealthBar;
+        unit.GetComponent<HealthBar>().healthBar=healthBar;
+        gameObjHealthBar.GetComponent<HealthSlider>().slider = gameObjHealthBar;
+        Canvas canvas = GameObject.FindGameObjectWithTag("CameraCanvas").GetComponent<Canvas>();
+        gameObjHealthBar.transform.SetParent( canvas.transform );
+    }
+}
---
+++ UnspokenTBS-Josh/UnspokenUnity/Assets/Switch Turns/SwitchTurnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTurnManager : MonoBehaviour
+{
+    int units;
+    public int currentId = 1;
+
+    //Switches player
+    public void switchID()
+    {
+        units = cube.numOfUnits;
+        if (currentId < units)
+            currentId++;
+        else
+        {
+            currentId = 1;
+        }
+        Debug.Log("CurrentID = " + currentId);
+        Debug.Log("Units = " + units);
+    }
+}
