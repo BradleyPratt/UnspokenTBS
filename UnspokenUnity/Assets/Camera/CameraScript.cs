bb062c7d0207ea89666a8c09df6cc02eaf7e1b3a
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2018-02-01 23:21:00 +0000

    Fixed camera

--- UnspokenUnity/Assets/Camera/CameraScript.cs
+++ UnspokenUnity/Assets/Camera/CameraScript.cs
@@ -3,48 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
-    public float speed = 1f;
+    public float speed = 2f;
+    public float zoomSpeed = 4f;
+
     float cameraDistanceMax = 20f;
     float cameraDistanceMin = 5f;
     float cameraDistance = 10f;
 
-    GameObject currentUnit;
-    GameObject lastUnit;
+    bool forwardBoundary = false;
+    bool leftBoundary = false;
+    bool downBoundary = false;
+    bool rightBoundary = false;
+
+    bool canMove = true;
+
+    GameObject boundary;
+
+    Vector3 boundaryRightPos;
+    Vector3 boundaryLeftPos;
+    Vector3 boundaryTopPos;
+    Vector3 boundaryBottomPos;
+    Vector3 boundaryForwardPos;
+    Vector3 boundaryBackPos;
 
     TurnManager turnManager;
 
     // Use this for initialization
     void Start() {
+        boundary = GameObject.Find("Boundary");
         turnManager = GameObject.Find("GameManager").GetComponent<TurnManager>();
+
+        Vector3 boundaryOffsetX = boundary.transform.right * (boundary.transform.localScale.x / 2f) * -1f;
+        Vector3 boundaryOffsetY = boundary.transform.up * (boundary.transform.localScale.y / 2f) * -1f;
+        Vector3 boundaryOffsetZ = boundary.transform.forward * (boundary.transform.localScale.z / 2f) * -1f;
+
+        boundaryLeftPos = boundary.transform.position + boundaryOffsetX;
+        boundaryRightPos = boundary.transform.position - boundaryOffsetX;
+        boundaryBottomPos = boundary.transform.position + boundaryOffsetY;
+        boundaryTopPos = boundary.transform.position - boundaryOffsetY;
+        boundaryBackPos = boundary.transform.position + boundaryOffsetZ;
+        boundaryForwardPos = boundary.transform.position - boundaryOffsetZ;
     }
 
     // Update is called once per frame
     void Update() {
-        Debug.Log(currentUnit);
-        Debug.Log(turnManager.GetCurrentUnit());
-        currentUnit = turnManager.GetCurrentUnit();
-
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) && transform.position.z <= 3500)
+        if (transform.position.z > boundaryForwardPos.z)
         {
-            //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);// * speed;
-            //Camera.main.transform.Translate(new Vector3(transform.position.y, transform.position.y, Vector3.forward.z) * speed);
+            transform.position = new Vector3(transform.position.x, transform.position.y, boundaryForwardPos.z);
+        }
+        if (transform.position.z < boundaryBackPos.z)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, boundaryBackPos.z);
+        }
+
+        if (transform.position.x < boundaryLeftPos.x)
+        {
+            transform.position = new Vector3(boundaryLeftPos.x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > boundaryRightPos.x)
+        {
+            transform.position = new Vector3(boundaryRightPos.x, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
             transform.position = new Vector3(transform.position.x + transform.forward.x, transform.position.y, transform.position.z + transform.forward.z);
         }
-        if (Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.A ) && transform.position.x >= -15000) {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
             transform.position = new Vector3(transform.position.x - transform.right.x, transform.position.y, transform.position.z - transform.right.z);
         }
-        if (Input.GetKey( KeyCode.DownArrow ) || Input.GetKey( KeyCode.S ) && transform.position.z >= -5000) {
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
             transform.position = new Vector3(transform.position.x - transform.forward.x, transform.position.y, transform.position.z - transform.forward.z);
         }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) && transform.position.x <= 24000)
-        {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
             transform.position = new Vector3(transform.position.x + transform.right.x, transform.position.y, transform.position.z + transform.right.z);
         }
-        if (Input.GetAxis("Mouse ScrollWheel")>0 && transform.position.y > 2) {
-            transform.position = new Vector3(transform.position.x, transform.position.y - speed*2, transform.position.z);
+
+        if (Input.GetAxis("Mouse ScrollWheel")>0 && transform.position.y > boundaryBottomPos.y) {
+            transform.position += transform.forward*zoomSpeed;
         }
-        else if (Input.GetAxis("Mouse ScrollWheel")<0 && transform.position.y < 100) {
-            transform.position = new Vector3(transform.position.x, transform.position.y + speed*2, transform.position.z);
+        else if (Input.GetAxis("Mouse ScrollWheel")<0 && transform.position.y < boundaryTopPos.y) {
+            transform.position -= transform.forward*zoomSpeed;
         }
 
         if (Input.GetKey( KeyCode.Q))
@@ -58,13 +96,5 @@ public class CameraScript : MonoBehaviour {
 
         cameraDistance += Input.GetAxis("Mouse ScrollWheel") * speed;
         cameraDistance = Mathf.Clamp(cameraDistance, cameraDistanceMin, cameraDistanceMax);
-
-        if (false)//(currentUnit != null && currentUnit != lastUnit)
-        {
-            Vector3 camPos = new Vector3 (currentUnit.transform.position.x, currentUnit.transform.position.y+3000, currentUnit.transform.position.z);
-            transform.position = camPos;
-        }
-
-        lastUnit = currentUnit;
     }
 }
