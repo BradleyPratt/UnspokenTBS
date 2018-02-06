cf13d6ed3dad9497f842a17fb990acde164abafc
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2018-02-06 21:11:14 +0000

    Fixed speed issue

--- UnspokenUnity/Assets/Camera/CameraScript.cs
+++ UnspokenUnity/Assets/Camera/CameraScript.cs
@@ -29,8 +29,16 @@ public class CameraScript : MonoBehaviour {
 
     TurnManager turnManager;
 
+    Resolution res;
+
     // Use this for initialization
     void Start() {
+        res = Screen.currentResolution;
+        if (res.refreshRate == 60)
+            QualitySettings.vSyncCount = 1;
+        if (res.refreshRate == 120)
+            QualitySettings.vSyncCount = 2;
+
         boundary = GameObject.Find("Boundary");
         turnManager = GameObject.Find("GameManager").GetComponent<TurnManager>();
 
