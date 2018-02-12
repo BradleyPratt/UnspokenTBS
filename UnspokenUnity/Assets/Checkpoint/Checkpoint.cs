06e305ba39093d63043559cd70cf6c848b52cfb4
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2018-02-12 17:39:43 +0000

    Updated checkpoint flags

--- UnspokenUnity/Assets/Checkpoint/Checkpoint.cs
+++ UnspokenUnity/Assets/Checkpoint/Checkpoint.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
     public float radius = 200;
+
+    bool redCheckpointBuilt;
+    bool blueCheckpointBuilt;
+
+    public GameObject checkpointRed;
+    public GameObject checkpointBlue;
 	
 	// Update is called once per frame
 	void Update () {
@@ -33,13 +39,37 @@ public class Checkpoint : MonoBehaviour {
         if ((j == 0 && k == 0) || j == k)
         {
             Debug.Log("Zone is neutral");
+            foreach (Transform child in transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+            redCheckpointBuilt = false;
+            blueCheckpointBuilt = false;
         }
         else if (j > k)
         {
             Debug.Log("The zone belongs to the US");
+            if (!blueCheckpointBuilt) {
+                foreach (Transform child in transform)
+                {
+                    GameObject.Destroy(child.gameObject);
+                }
+                Instantiate(checkpointBlue, transform);
+            }
+            blueCheckpointBuilt = true;
+            redCheckpointBuilt = false;
         }
         else {
             Debug.Log("The zone belongs to the USSR");
+            if (!redCheckpointBuilt) {
+                foreach (Transform child in transform)
+                {
+                    GameObject.Destroy(child.gameObject);
+                }
+                Instantiate(checkpointRed, transform);
+            }
+            redCheckpointBuilt = true;
+            blueCheckpointBuilt = false;
         }
     }
 
