da4800f8c8ed123f32aded4397f81445caa69407
MortalArrow <githubstuff@fireshard.net>
2018-02-26 18:48:00 +0000

    Revert "Fixing overwriting of newer code."
    This reverts commit d830da3060a2bd90bb1d8e7b755f0963818550f3.

---
+++ UnspokenUnity/Assets/Money/Money.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour {
+    TurnManager turnManager;
+
+    string activeTeam;
+
+    float USMoney = 800;
+    float USSRMoney = 800;
+
+    Text moneyText;
+
+	// Use this for initialization
+	void Start () {
+        turnManager = GameObject.Find("GameManager").GetComponent<TurnManager>();
+        moneyText = GameObject.Find("MoneyText").GetComponent<Text>();
+
+		UpdateMoneyUI();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+	// true if money adjusted correctly, otherwise false.
+    public bool SetUSMoney(float amount)
+    {
+		if ((USMoney + amount) >= 0)
+		{
+			USMoney = USMoney + amount;
+			UpdateMoneyUI();
+			return true;
+		}
+        return false;
+    }
+
+	// true if money adjusted correctly, otherwise false.
+	public bool SetUSSRMoney(float amount)
+	{
+		if ((USSRMoney + amount) >= 0)
+		{
+			USSRMoney = USSRMoney + amount;
+			UpdateMoneyUI();
+			return true;
+		}
+		return false;
+    }
+
+    public float GetUSMoney()
+    {
+        return USMoney;
+    }
+
+    public float GetUSSRMoney()
+    {
+        return USSRMoney;
+    }
+
+	public void UpdateMoneyUI()
+	{
+		if (activeTeam == "USA")
+		{
+			moneyText.text = USMoney.ToString();
+		}
+		else
+		{
+			moneyText.text = USSRMoney.ToString();
+		}
+	}
+}
