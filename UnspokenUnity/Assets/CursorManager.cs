94cd234195310b0deceb9a1e094c0335f1f73471
MortalArrow <githubstuff@fireshard.net>
2018-02-28 23:18:35 +0000

    Renamed class.

--- UnspokenUnity/Assets/CursorManager.cs
+++ UnspokenUnity/Assets/CursorManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cursor : MonoBehaviour {
+public class CursorManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
