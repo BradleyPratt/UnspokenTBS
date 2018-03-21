cbe495fe4c9263ca849a8ab3c5a2a614ab566991
MortalArrow <githubstuff@fireshard.net>
2018-03-21 12:59:45 +0000

    - Now checks if the raycast from the cursor hits something or not - Improved comments.

--- UnspokenUnity/Assets/MiniMap/MiniMapUnitIcon.cs
+++ UnspokenUnity/Assets/MiniMap/MiniMapUnitIcon.cs
@@ -7,6 +7,7 @@ public class MiniMapUnitIcon : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		// Make object become independant, apply scaling, then re-attach to parent.
+		// This means that their scalling takes into account the parent object's scale, and compensates for it.
 		Transform parent = transform.parent;
 		transform.parent = null;
 		transform.localScale = new Vector3(16,1,16);
@@ -18,6 +19,7 @@ public class MiniMapUnitIcon : MonoBehaviour {
 		
 	}
 
+	// Change the colour of the MeshRenderer
 	public void SetColor(Color newColor)
 	{
 		this.GetComponent<MeshRenderer>().material.color = newColor;
